5340e1b07edceae7f102da8df6046f57c4765e7f
Victoria LeBel <vickileb@hotmail.com>
2022-08-02 16:25:35 -0700

    added more get and delete endpoints

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
@@ -8,7 +10,7 @@ class ApplicationController < Sinatra::Base
 
   get "/houses" do
     houses = House.all
-    houses.to_json(include: :address)
+    houses.to_json(include: [:owner, :address])
   end
 
   get "/houses/:id" do
@@ -16,4 +18,39 @@ class ApplicationController < Sinatra::Base
     house.to_json(include: {rooms: {include: {projects: {include: [:tools, :parts]}}}})
   end
 
+  get "/houses/:id/projects" do
+    House.find(params[:id]).projects.to_json
+  end
+
+  get "/houses/:id/projects/tools" do
+    house = House.find(params[:id]).projects
+    house.to_json(include: :tools)
+  end
+
+  get "/houses/:id/projects/parts" do
+    house = House.find(params[:id]).projects
+    house.to_json(include: :parts)
+  end
+
+  delete "/projects/:id" do
+    project = Project.find(params[:id])
+    project.destroy
+    project.to_json
+  end
+
+  delete "/parts/:id" do
+    part= Part.find(params[:id])
+    part.destroy
+    part.to_json
+  end
+
+  delete "/tools/:id" do
+    tool= Tool.find(params[:id])
+    tool.destroy
+    tool.to_json
+  end
+
+
+
 end
+
